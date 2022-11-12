01735d0f62bed6c64a6077e9fa6077bbe2366007
seo2im <seo2im6492@gmail.com>
2022-11-12 15:30:30 +0900

    ✨ client(app) 기본 환경 세팅

---
+++ assistant_app/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'airbnb', 'airbnb', 'airbnb-typescript'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        "import/prefer-default-export": 0,
+        'no-undef': 'off',
+      },
+    },
+  ],
+};
